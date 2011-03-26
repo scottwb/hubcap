3b4ebede781bbf3a1c31272ca57b39cea3152592
Scott W. Bradley <scottwb@scottwb.net>
2011-03-26 12:04:16 -0700

    Split out Octopi monkey patches into a separate file.

--- lib/hubcap.rb
+++ lib/hubcap.rb
@@ -3,29 +3,8 @@
 # Docs: https://github.com/fcoury/octopi
 #
 require 'octopi'
+require 'octopi_ext'
 require 'ap'
-
-module Octopi
-  class Issue
-    def self.labels(options = {})
-      ensure_hash(options)
-      user, repo = gather_details(options)
-      Api.api.get("/issues/labels/#{user}/#{repo}")
-    end
-
-    def self.add_label(options = {})
-      ensure_hash(options)
-      user, repo = gather_details(options)
-      Api.api.get("/issues/label/add/#{user}/#{repo}/#{options[:label]}")
-    end
-
-    def self.del_label(options = {})
-      ensure_hash(options)
-      user, repo = gather_details(options)
-      Api.api.get("/issues/label/remove/#{user}/#{repo}/#{options[:label]}")
-    end
-  end
-end
 
 module Hubcap
   extend Octopi
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
 require 'bundler/setup'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'hubcap'))
+require File.expand_path(File.dirname(__FILE__) + '/../lib/hubcap')
 require 'ap'
 
